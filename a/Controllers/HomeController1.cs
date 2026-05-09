39f6e8ff325b7a98edde8083530f153d186abba0
javifabi <j.fabia@alumnos.santotomas.cl>
2026-05-09 18:33:40 -0400

    Create HomeController1.cs

---
+++ a/Controllers/HomeController1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace a.Controllers
+{
+    public class HomeController1 : Controller
+    { }
+
+namespace a.Controllers
+    {
+        public class ProductosController : Controller
+        {
+            public IActionResult Index()
+            {
+                var productos = new List<dynamic>
+            {
+                new { Id = 1, Nombre = "Mouse", Precio = 10000 },
+                new { Id = 2, Nombre = "Teclado", Precio = 20000 },
+                new { Id = 3, Nombre = "Monitor", Precio = 150000 }
+            };
+
+                return View(productos);
+            }
+        }
+    }
+}
