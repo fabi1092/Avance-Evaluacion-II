b510210afc131d2e49df120edac51e1aa9616015
fabi1092 <fabianjosebf@gmail.com>
2026-05-08 21:06:12 -0400

    trabajo

---
+++ a/Models/Class.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+
+namespace a.Models
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string rut { get; set; }
+        public DateTime Nacimiento { get; set; }
+        public float Peso { get; set; }
+        public string correo { get; set; }
+        public bool alergias { get; set; }
+        public string TipoSangre { get; set; }
+        public bool Prevision { get; set; } 
+
+    }
+}
